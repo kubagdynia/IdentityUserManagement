868b997bb72a1a6bea7d3330168964e957de8912
kubagdynia <kubagdynia@gmail.com>
2024-10-13 12:21:02 +0200

    Added initial project structures

---
+++ IdentityUserManagement.Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddEndpointsApiExplorer() // Add the endpoint API explorer for generating OpenAPI document.
+    .AddSwaggerGen(opt => opt.CustomSchemaIds(t => t.FullName?.Replace('+', '.'))); // Add Swagger generator
+
+// Build the application
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwaggerUI(); // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+
+}
+
+app.UseHttpsRedirection(); // Redirects HTTP requests to HTTPS.
+
+app.MapGet("/test", (string name) => $"Hello, {name}!"); // Map the GET request to the path "/test" to the lambda function that returns a string.
+
+app.Run();
---
+++ IdentityUserManagement.Core.Tests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿namespace IdentityUserManagement.Core.Tests;
+
+public class UnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
